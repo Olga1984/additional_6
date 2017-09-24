92f51bff172b1f9da7c074cc5744c4c08f095c58
Olga1984 <noa84@tut.by>
2017-09-24 03:19:37 +0300

    zeros: function factorial 1step and 2, arr working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function zeros(expression) {
-  // your solution
+    // your solution
+    function factorialSimple(n) {
+        if (n === 0) {
+            return 1;
+        }
+        return n * factorialSimple(n - 1);
+    }
+
+    function factorialminTwo(n) {
+        if (n === 0) {
+            return 1;
+        }
+        return n * factorialminTwo(n - 2);
+    }
+    var arr = expression.split("*");
+    //console.log(arr);
+    for (i = 0; i < arr.length; i++) {
+        var fitem = arr[i];
+        //console.log(fitem);
+        var fitem_p = parseInt(fitem);
+        //console.log(fitem_p);
+        if (fitem_p) {
+            var Clo = fitem.lastindexOf('!',2);
+            //console.log(Clo);
+            if (Clo !== -1) {
+                //factorialSimple(fitem_p);
+            } else {
+                //factorialminTwo(fitem_p);
+            }
+
+        }
+
+    }
 }
+
+
