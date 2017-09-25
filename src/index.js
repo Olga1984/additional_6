c489c0d8793220c5be926ec7b151068a21fdc887
Olga1984 <noa84@tut.by>
2017-09-25 04:47:39 +0300

    zeros: var mult and res in for loop

--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
 module.exports = function zeros(expression) {
-    // your solution
-    function factorialSimple(n) {
-        if (n === 0) {
-            return 1;
-        }
-        return n * factorialSimple(n - 1);
-    }
-
-    function factorialminTwo(n) {
-        if (n === 0) {
-            return 1;
-        }
-        return n * factorialminTwo(n - 2);
-    }
-    var arr = expression.split("*");
-    //console.log(arr);
-    for (i = 0; i < arr.length; i++) {
-        var fitem = arr[i];
-        //console.log(fitem);
-        var fitem_p = parseInt(fitem);
-        //console.log(fitem_p);
-        if (fitem_p) {
-            var Clo = fitem.lastindexOf('!',2);
-            //console.log(Clo);
-            if (Clo !== -1) {
-                //factorialSimple(fitem_p);
-            } else {
-                //factorialminTwo(fitem_p);
-            }
-
+        // your solution
+        function factorialSimple(n) {
+            return n ? n * factorialSimple(n - 1) : 1;
         }
 
-    }
-}
-
-
+        //function factorialminTwo(n) {            
+        //       return n ? n * factorialSimple(n - 2) : 1;
+       // }
+        var arr = expression.split("*");
+        //console.log(arr);
+        var mult=1;
+        var res;
+        for (i = 0; i < arr.length; i++) {
+            var fitem = arr[i];
+            //console.log(fitem);
+            var x = parseInt(fitem); //number
+            //console.log(x);
+            var l = fitem.lastIndexOf('!'); //count for !
+            //console.log(l);
+            if (x < 10) {
+                if (l === 1) {
+                    res = factorialSimple(x);
+                } else {
+                    res = factorialminTwo(x);
+                }
+            } //1
+            else if (10 <= x < 100) {
+                if (l === 2) {
+                    res = factorialSimple(x);
+                } else {
+                    res = factorialminTwo(x);
+                }
+            } //2
+            else if (x = 100) {
+                if (l === 3) {
+                    res = factorialSimple(x);
+                } else {
+                    res = factorialminTwo(x);
+                }
+            } //3
+            mult*=res;
+        } //for
+        console.log(mult);
+   var str = mult+"";    
+   //console.log(str);
+   //console.log(typeof(str));
+    
+    //zeros count
+    var zeroscount = 0;
+    var zero;
+    var regexp = /0/g;
+    var matches_array = str.match(regexp);
+      for (k = 0; k < matches_array.length; k++){
+         zero = matches_array[k];
+          //console.log(zero);          
+        zeroscount++;         
+      }//for     
+    return zeroscount;
+    } //zeros
